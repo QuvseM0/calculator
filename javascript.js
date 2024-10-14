e916fbe1120f0e5ae33e3a0a095ccb472d453f36
quvse <marquesoafonso@gmail.com>
2024-10-14 22:56:57 +0100

    added keyboard support

--- javascript.js
+++ javascript.js
@@ -82,7 +82,6 @@ floatButton.addEventListener("click", e => {
 
 equal.addEventListener("click", e => { 
     let result = operate(numA,opSelected,numB)
-    
     if(!Number.isInteger(result)){
         displayValue = Math.round(result * 100) / 100
     }else{
@@ -112,4 +111,64 @@ bckspace.addEventListener("click", e => {
     display.textContent = display.textContent.slice(0, -1)
 })
 
+window.addEventListener("keydown", event => {
+    if(event.key == "Enter"){
+        let result = operate(numA,opSelected,numB)
+        if(!Number.isInteger(result)){
+            displayValue = Math.round(result * 100) / 100
+        }else{
+            displayValue = result
+        }
+        if(displayValue === Infinity){
+            display.textContent = "LIGMA!"
+        }else if (isNaN(displayValue)){
+            display.textContent = "0"
+            displayText = ""
+            numA, numB, displayValue = 0    
+        }else{
+            display.textContent = displayValue
+        }
+        numA = Number(displayValue)
+        numB = 0
+        opSelected = ""
+    }
+    if(event.key == "Backspace"){
+        display.textContent = "0"
+        displayText = ""
+        numA, numB, displayValue = 0
+    }
+    if(event.key == "."){
+        text = floatButton.textContent
+        while(!display.textContent.includes(".")){
+            if(display.textContent === opSelected){
+                display.textContent = "0"
+                display.textContent += text
+            }else{
+                display.textContent += text
+            }
+        }
+    }
+    if(event.key >= 0 && event.key <= 9){
+        if(display.textContent === "0" || ops.includes(display.textContent)){display.textContent = ""}
+        let range = [0,1,2,3,4,5,6,7,8,9]
+        text = range[event.key]
+        displayText = display.textContent
+        if (display.textContent.length <= 9){
+            display.textContent += text
+        }else{
+            displayText
+        }
+        if(opSelected == ""){
+            numA = Number(displayText + text)
+        }else{
+            numB = Number(displayText + text)
+        }
+    }
+    if(ops.includes(event.key)){
+        text = event.key 
+        display.textContent = text
+        opSelected = text
+    }
+    }
+)
 
